e6828201f99101877af6f6dab9c279ac13c0b163
MichaelHuu <michaelhuu1809@gmail.com>
2023-03-18 16:20:55 +0100

    updates

--- javascript/articles/articlesloader.js
+++ javascript/articles/articlesloader.js
@@ -4,21 +4,38 @@ fetch('articles.json')
     const articlesContainer = document.getElementById('articles-container');
     data.articles.forEach(article => {
       const articleCard = document.createElement('div');
-      articleCard.className = 'col-lg-4 col-md-6 mb-4';
+      articleCard.className = '';
+      let tagsHTML = ''; // initialize empty string for tags
+      if (article.tags.length > 0) { // check if tags exist
+        for (let i = 0; i < article.tags.length; i++) {
+          tagsHTML += `<a href="#" class="btn btn-secondary btn-lg disabled catergory_tag" role="button" aria-disabled="true">${article.tags[i]}</a>`;
+        }
+      }
+
+      // <a href="article.html?id=${article.ID}" class="basic"">
+      
       articleCard.innerHTML = `
-      <a href="article.html?id=${article.ID}" class="basic"">
-        <div class="card h-100">
-          <div class="card-body">
-            <h4 class="card-title">${article.title}</h4>
-            <h6 class="card-subtitle mb-2 text-muted">${article.author}</h6>
-            <p class="card-text">${article.content}</p>
+      
+      <a href="#" class="basic " onclick="changeCardTextHeight('${article.ID}'); event.preventDefault();">
+          <div id="card-${article.ID}" class="card ">
+            <div class="card-body col">
+              <h4 class="card-title">${article.title}</h4>
+              <h6 class="card-subtitle mb-2 text-muted">${article.author}</h6>
+              <p id="card-text-${article.ID}" class="card-text">${article.content}</p>
+            </div>
+            <div class="card-footer">
+              <div class="footer-left">
+                <p class="date_text">March 17, 2023</p>
+              </div>
+              <div class="footer-right">
+                <div id="tag-container">
+                  ${tagsHTML} <!-- insert tags HTML here -->
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="card-footer">
-            <small class="text-muted righttext">${article.date}</small>
-          </div>
-        </div>
-      </a>
-    `;
+        </a>
+      `;
       articlesContainer.appendChild(articleCard);
     });
   })
@@ -26,4 +43,25 @@ fetch('articles.json')
 
 
 
+
+  function changeCardTextHeight(id) {
+    const cardTextonid = document.getElementById("card-text-"+id);
+    const cardText = document.getElementById("card-text-"+id);
+    const cardBodies = document.querySelectorAll(".card");
+    const cardID =document.getElementById("card-"+id)
+
+    if (cardTextonid) {
+      cardTextonid.style.height = "auto";
+      cardTextonid.style.maxHeight = "none";
+      // for (let i = 0; i < cardBodies.length; i++) {
+
+      //   console.log("current: " + cardBodies[i].style.width)
+      //   cardBodies[i].style.width = "30vw";
+      //   console.log("changed width of " + cardBodies[i]);
+      //   console.log("to: " + cardBodies[i].style.width)
+
+      // }
+    }
+
+  }
   
